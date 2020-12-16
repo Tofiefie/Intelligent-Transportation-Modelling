9cefe09db941c54aef6e3cedef22aeea89631224
Tofiefie <119467598+Tofiefie@users.noreply.github.com>
2020-12-16 07:27:01 +0000

    update results

--- PAZHOU/base/data/build.py
+++ PAZHOU/base/data/build.py
@@ -278,4 +278,145 @@ def build_reid_train_imagenet_loader_lazy(
     logger = logging.getLogger(__name__)
     logger.info("Using training sampler {}".format(sampler_name))
     assert sampler_name == "TrainingSampler"
-    sampler = samplers.Training
+    sampler = samplers.TrainingSampler(train_set)
+
+    batch_sampler = paddle.io.BatchSampler(sampler=sampler, batch_size=mini_batch_size, drop_last=True)
+    train_loader = paddle.io.DataLoader( #TODO make a distributed version
+        dataset=train_set,
+        batch_sampler=batch_sampler,
+        collate_fn=mix_collate_fn,
+        num_workers=num_workers,
+        )
+
+    return train_loader
+
+
+def build_cocodet_test_loader_lazy(data_set, total_batch_size=0, num_workers=0, is_train=False,
+        batch_transforms=[], shuffle=True, drop_last=True, collate_batch=True):
+    """
+    Build a dataloader for coco detection with some default features.
+
+    Returns:
+        paddle.io.DataLoader: a dataloader.
+    """
+    assert is_train == False
+    num_classes = 80    # hard code
+    batch_transforms = BatchCompose(batch_transforms, num_classes, collate_batch)
+    batch_sampler = paddle.io.BatchSampler(dataset=data_set, batch_size=total_batch_size, drop_last=False, shuffle=False)
+    data_loader = paddle.io.DataLoader(
+        dataset=data_set,
+        batch_sampler=batch_sampler,
+        collate_fn=batch_transforms,
+        num_workers=num_workers,
+        return_list=False,
+        use_shared_memory=False,
+    )
+    return data_loader
+
+def build_cocodet_loader_lazy(data_set, total_batch_size=0, num_workers=0, is_train=False,
+        batch_transforms=[], shuffle=True, drop_last=True, collate_batch=True):
+    """
+    Build a dataloader for coco detection with some default features.
+
+    Returns:
+        paddle.io.DataLoader: a dataloader.
+    """
+    mini_batch_size = total_batch_size // comm.get_world_size()
+    num_classes = 80    # hard code
+    batch_transforms = BatchCompose(batch_transforms, num_classes, collate_batch)
+    
+    if is_train:
+        # 无限流
+        sampler = samplers.TrainingSampler(data_set, shuffle=shuffle)
+    else:
+        # 有序分布流
+        sampler = samplers.OrderInferenceSampler(data_set, mini_batch_size)
+    
+    batch_sampler = paddle.io.BatchSampler(sampler=sampler, batch_size=mini_batch_size)
+    data_loader = paddle.io.DataLoader(
+        dataset=data_set,
+        batch_sampler=batch_sampler,
+        collate_fn=batch_transforms,
+        num_workers=num_workers,
+        return_list=False,
+        use_shared_memory=False,
+    )
+    
+    return data_loader
+
+
+def build_cocodet_set(dataset_name=None, transforms=[], dataset_dir=_root, image_dir='train2017',
+        anno_path='annotations/instances_train2017.json',
+        is_padding=False,
+        data_fields=['image', 'gt_bbox', 'gt_class', 'is_crowd'], **kwargs):
+    """
+    build train_set for detection
+    """
+    data_set = DATASET_REGISTRY.get(dataset_name)(dataset_dir=os.path.join(dataset_dir, 'coco'), image_dir=image_dir,
+        anno_path=anno_path, data_fields=data_fields)
+    num_classes = 80    # hard code
+    transforms = Compose(transforms, num_classes=num_classes)
+    data_set.parse_dataset()
+    data_set.set_transform(transforms)
+    data_set.set_kwargs(**kwargs)
+    print('{} has {} samples'.format(dataset_name, len(data_set))) #data_set.roidbs
+    if is_padding:
+        # record sample number
+        data_set.num_valid_samples = len(data_set)
+        # 在末尾随机填充若干data.gallery数据使得test_items的长度能被world_size整除，以保证每个卡上的数据量均分；
+        test_items = data_set.roidbs
+        world_size = comm.get_world_size()
+        if len(test_items)%world_size != 0:
+            idx_list = list(range(len(test_items)))
+            random_idx_list = [random.choice(idx_list) for _ in range(world_size - len(test_items)%world_size)]
+            test_items += [test_items[idx] for idx in random_idx_list]
+        data_set.roidbs = test_items
+        print('{} has {} samples after padding'.format(dataset_name, len(data_set))) #data_set.roidbs
+    return data_set
+
+def build_voc_set(dataset_name=None, transforms=[], dataset_dir=_root, image_dir='voc',
+        anno_path='trainval.txt',
+        data_fields=['image', 'gt_bbox', 'gt_class', 'difficult'], **kwargs):
+    """
+    build train_set for detection
+    """
+    import ppdet
+    data_set = ppdet.data.voc.VOCDataSet(dataset_dir=os.path.join(dataset_dir, image_dir),
+        anno_path=anno_path, label_list='label_list.txt', data_fields=data_fields)
+    num_classes = 20    # hard code
+    transforms = Compose(transforms, num_classes=num_classes)
+    data_set.parse_dataset()
+    data_set.set_transform(transforms)
+    data_set.set_kwargs(**kwargs)
+    data_set.dataset_name = dataset_name
+    
+    # to use coco evaluator, add catid2clsid and get_anno()
+    data_set.catid2clsid = dict({catid: i for i, catid in 
+                enumerate([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20])
+                })
+    def get_anno():
+        return os.path.join(dataset_dir, image_dir, 'test.cocostyle.json')
+    data_set.get_anno = get_anno
+    print('{} has {} samples'.format(dataset_name, len(data_set))) #data_set.roidbs
+    return data_set
+
+def build_obj365det_set(dataset_name=None, transforms=[], dataset_dir=_root, image_dir='train2017',
+        anno_path='annotations/instances_train2017.json',
+        data_fields=['image', 'gt_bbox', 'gt_class', 'is_crowd'], **kwargs):
+    """
+    build train_set for detection
+    """
+    import ppdet
+    data_set = ppdet.data.COCODataSet(
+        dataset_dir=os.path.join(dataset_dir, 'Objects365'), 
+        image_dir=image_dir,
+        anno_path=anno_path, 
+        data_fields=data_fields)
+    num_classes = 365   # hard code
+    transforms = Compose(transforms, num_classes=num_classes)
+    data_set.parse_dataset()
+    data_set.set_transform(transforms)
+    data_set.set_kwargs(**kwargs)
+    data_set.dataset_name = dataset_name
+    print('{} has {} samples'.format(dataset_name, len(data_set))) #data_set.roidbs
+    return data_set
